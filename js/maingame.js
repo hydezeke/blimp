65b9a4c24f442978b7a8c83790b316b08abd23e5
Michael <savben@gmail.com>
2016-04-07 15:03:55 -0400

    res
    res

--- js/maingame.js
+++ js/maingame.js
@@ -11,7 +11,7 @@ window.onload = function() {
     // loading functions to reflect where you are putting the assets.
     // All loading functions will typically all be found inside "preload()".
     
-   var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
+   var game = new Phaser.Game(800, 450, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
 
     function preload() {
 
